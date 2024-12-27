591692d588605b07afa3c162e0e16e729b24f0f7
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-27 17:41:45 +1300

    Add operate function
    Operate will call on the operator's corresping function

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,33 @@
-const add = function(a, b) {
-    return a + b;
+let leftOperand = 0;
+let rightOperand = 0;
+let operator;
+
+
+const add = function(leftOperand, rightOperand) {
+    return leftOperand + rightOperand;
 };
 
-const subtract = function(a, b) {
-    return a - b;
+const subtract = function(leftOperand, rightOperand) {
+    return leftOperand - rightOperand;
 };
 
-const multiply = function(a, b) {
+const multiply = function(leftOperand, rightOperand) {
     // return arr.reduce((total, num) => total * num);
-    return a * b;
+    return leftOperand * rightOperand;
 };
 
-const divide = function(a, b) {
-    return a / b;
+const divide = function(leftOperand, rightOperand) {
+    return leftOperand / rightOperand;
 };
+
+const operate = function(operator, leftOperand, rightOperand) {
+    return operator === '+' ? add(leftOperand, rightOperand)
+    : operator === '-' ? subtract(leftOperand, rightOperand)
+    : operator === '*' ? multiply(leftOperand, rightOperand)
+    : operator === '/' ? divide(leftOperand, rightOperand)
+    : "Not a valid operator";
+}
+
+
+// Manual testing
+console.log(operate('+', 3, 4));
