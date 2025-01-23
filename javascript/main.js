96937bb6f398c396e3688905a7b47c8b03dd65bc
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2025-01-23 16:45:36 +1300

    Register digit buttons on screen
    Added values to number buttons
    Added event listener to numPad div
    Added functions layout for each kind of operation

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,13 @@
+const keyboard = document.getElementById("keyboard");
+const operands = document.querySelectorAll(".operand");
+const numPad = document.getElementById("numPad");
+
+let operator;
 let leftOperand = 0;
 let rightOperand = 0;
-let operator;
+
+let lastOperation = document.getElementById("lastOperationDisplay");
+let currentOperation = document.getElementById("currentOperationDisplay");
 
 const add = function(leftOperand, rightOperand) {
     return leftOperand + rightOperand;
@@ -26,6 +33,39 @@ const operate = function(operator, leftOperand, rightOperand) {
     : "Not a valid operator";
 }
 
+const populateDisplay = function() {
+    numPad.addEventListener("click", (e) => registerOperands(e));
+    
+    // clearDisplay();
+}
+
+const clearDisplay = function() {
+    lastOperation.innerText = "";
+    currentOperation.innerText = "";
+}
+
+
+const backspaceDisplay = function () {
+
+}
+
+
+const registerOperands = function (e) {
+    currentOperation.innerText += e.target.value;
+}
+
+
+const registerOperator = function() {
+
+}
+
+const registerOperation = function() {
+
+
+}
+
 
 // Manual testing
-console.log(operate('+', 3, 4));
+// console.log(operate('+', 3, 4));
+
+populateDisplay();
