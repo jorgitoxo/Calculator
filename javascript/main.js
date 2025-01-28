c62b1b08c6a7621376d32fdf11898ca0f22eb3d7
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2025-01-29 12:53:55 +1300

    Display operations
    Registering operator
    Renamed function to calculator

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,14 @@
-// const keyboard = document.getElementById("keyboard");
-// const operands = document.querySelectorAll(".operand");
+let operator = "";
 let lastOperation = document.getElementById("lastOperationDisplay");
 let currentOperation = document.getElementById("currentOperationDisplay");
 
+let leftOperand = parseInt(lastOperation.innerText);
+let rightOperand = parseInt(currentOperation.innerText);
 
 const add = (leftOperand, rightOperand) => leftOperand + rightOperand;
-
 const subtract = (leftOperand, rightOperand) => leftOperand - rightOperand;
-
 const multiply = (leftOperand, rightOperand) => leftOperand * rightOperand;
-
 const divide = (leftOperand, rightOperand) => leftOperand / rightOperand;
-
 const operate = (operator, leftOperand, rightOperand) => {
     return operator === '+' ? add(leftOperand, rightOperand)
     : operator === '-' ? subtract(leftOperand, rightOperand)
@@ -20,51 +17,62 @@ const operate = (operator, leftOperand, rightOperand) => {
     : "Not a valid operator";
 }
 
-
-const populateDisplay = function() {
-    let operator;
-    let leftOperand = 0;
-    let rightOperand = 0;
-
-    // Operands
-    registerOperands();
-    
-    // Operators
-
-    // Operations
-    registerOperation();
+const clearDisplay = function() {
+    lastOperation.innerText = "";
+    currentOperation.innerText = "";
 }
 
-
-const registerOperands = function (e) {
-    const numPad = document.getElementById("numPad");
-    numPad.addEventListener("click", (e) => currentOperation.innerText += e.target.value);
+const backspaceDisplay = function() {
+    currentOperation.innerText = currentOperation.innerText.slice(0, -1);
 }
 
-const registerOperation = function() {
+const updateResult = () => {
+    leftOperand = parseInt(lastOperation.innerText);
+    rightOperand = parseInt(currentOperation.innerText);
+    let operationResult = 0;
+
+    clearDisplay();
+    operationResult = operate(operator, leftOperand, rightOperand);
+    currentOperation.innerText = operationResult;
+}
+
+const updateOperator = (e) => {
+    // lastOperation.innerText = currentOperation.innerText
+    operator = e.target.innerText;
+    leftOperand = parseInt(lastOperation.innerText);
+    rightOperand = parseInt(currentOperation.innerText);
+
+    operationResult = operate(operator, leftOperand, rightOperand);
+    lastOperation.innerText = operationResult;
+
+}
+
+const calculator = function() {
+    // Display operations
     const clear = document.getElementById("clear");
+    clear.addEventListener("click", () => clearDisplay(lastOperation, currentOperation));
     const backspace = document.getElementById("backspace");
+    backspace.addEventListener("click", () => backspaceDisplay(currentOperation));
 
-    const clearDisplay = function() {
-        lastOperation.innerText = "";
-        currentOperation.innerText = "";
-    }
 
-    const backspaceDisplay = function () {
-        currentOperation.innerText = currentOperation.innerText.slice(0, -1);
-    }
-    
-    clear.addEventListener("click", () => clearDisplay());
-    backspace.addEventListener("click", () => backspaceDisplay())
+    // Register operands on screen
+    const numPad = document.getElementById("numPad");
+    numPad.addEventListener("click", (e) => {
+        if (e.target.value !== undefined && e.target.value !== "=") {
+            currentOperation.innerText += e.target.value
+        } else if (e.target.value === "=") {
+            updateResult();
+        }
+    });
+
+
+    // Operators
+    const operatorPad = document.getElementById("operators");
+    operatorPad.addEventListener("click", (e) => {
+        // operator = e.target.innerText;
+        updateOperator(e);
+        // currentOperation.innerText = ;
+    });
 }
 
-
-
-const registerOperator = function() {
-
-}
-
-// Manual testing
-// console.log(operate('+', 3, 4));
-
-populateDisplay();
+calculator();
