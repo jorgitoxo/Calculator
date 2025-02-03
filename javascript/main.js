40237b4fbae72a3289395f258b4762888d43cdd9
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2025-02-03 21:55:29 +1300

    Fix backspace button and division by zero
    Added flag endOfOperationFlag to know when there is a division by zero

--- javascript/main.js
+++ javascript/main.js
@@ -2,16 +2,17 @@ let operator = '';
 let leftOperand = '';
 let rightOperand = '';
 let operationResult = '';
+let endOfOperationFlag = true;
 
-let lastOperation = document.getElementById("lastOperationDisplay");
-let currentOperation = document.getElementById("currentOperationDisplay");
+const lastOperation = document.getElementById("lastOperationDisplay");
+const currentOperation = document.getElementById("currentOperationDisplay");
 
 const clearButton = document.getElementById("clear");
 const backspaceButton = document.getElementById("backspace");
-const decimalButton = document.getElementById("decimalSeparator");
-const equalsButton = document.getElementById("equalsButton");
 
 const numPad = document.getElementById("numPad");
+const decimalButton = document.getElementById("decimalSeparator");
+const equalsButton = document.getElementById("equalsButton");
 const operatorPad = document.getElementById("operators");
 
 const add = (leftOperand, rightOperand) => leftOperand + rightOperand;
@@ -28,50 +29,72 @@ const operate = (operator, leftOperand, rightOperand) => {
 }
 
 const clearDisplay = () => {lastOperation.innerText = '', currentOperation.innerText = ''};
-const backspaceDisplay = () => currentOperation.innerText = currentOperation.innerText.slice(0, -1);
-const clearValues = () => {operator = '', leftOperand = '', rightOperand = '', operationResult = ''};
+
+const clearValues = () => {operator = '', leftOperand = '', rightOperand = ''};
+
+const backspaceDisplay = () => {
+    resetFlag();
+    if (currentOperation.innerText !== '') { 
+        currentOperation.innerText = currentOperation.innerText.slice(0, -1)
+        updateOperand();
+    }
+};
 
 const executeOperation = function() {
-    if (leftOperand === '')
-        leftOperand = 0;
-    if (operator && rightOperand === '')
-        rightOperand = 0;
-
-    clearDisplay();
+    if (leftOperand === '') leftOperand = 0;
+    if (operator && rightOperand === '') rightOperand = 0;
 
     if (operator === '') {
         currentOperation.innerText = leftOperand;
+    } else if (operator === "/" && rightOperand === 0) {
+        lastOperation.innerText = '';
+        setFlag();
+        endOfOperationFlag = true;
     } else {
-        leftOperand = operate(operator, leftOperand, rightOperand);;
+        leftOperand = operate(operator, leftOperand, rightOperand);
+        // operationResult = operate(operator, leftOperand, rightOperand);
+        // leftOperand = operationResult
         currentOperation.innerText = leftOperand;
+        lastOperation.innerText = '';
         rightOperand = '';
         operator = '';
+        // endOfOperationFlag = true;
     }
 }
 
 const updateOperator = (e) => {
-    clearDisplay();
-    
-    if (rightOperand !== '') {
-        operationResult = operate(operator, leftOperand, rightOperand);
-        leftOperand = operationResult;
+    if (rightOperand === '') {
+        // leftOperand = operationResult;
         operator = e.target.value;
         lastOperation.innerText = leftOperand + operator;
-        rightOperand = '';
+        currentOperation.innerText = '';
+    } else if (rightOperand === 0 && operator === "/") {
+        setFlag();
     } else {
+        leftOperand = operate(operator, leftOperand, rightOperand);
         operator = e.target.value;
         lastOperation.innerText = leftOperand + operator;
+        currentOperation.innerText = '';
+        rightOperand = '';
     }
 }
 
 const updateOperand = function(e) {
-    currentOperation.innerText += e.target.value;
-
     if (operator === '')
-        leftOperand = parseInt(currentOperation.innerText);
+        leftOperand = currentOperation.innerText ? parseInt(currentOperation.innerText) : 0;
     else
-        rightOperand = parseInt(currentOperation.innerText);
+        rightOperand = currentOperation.innerText ? parseInt(currentOperation.innerText) : 0;
 }
+
+const resetFlag = () => {
+    if (endOfOperationFlag) {
+        clearDisplay();
+        clearValues();
+        endOfOperationFlag = false;
+    }
+}
+
+const setFlag = () => {currentOperation.innerText = "You, naughty you!!", endOfOperationFlag = true};
 
 const calculator = function() {
     // Operations
@@ -82,8 +105,11 @@ const calculator = function() {
 
     // Operands
     numPad.addEventListener("click", (e) => {
-        if (e.target.value !== undefined)
+        if (e.target.value !== undefined) {
+            resetFlag();
+            currentOperation.innerText += e.target.value;
             updateOperand(e);
+        }
     });
 
     // Operators
